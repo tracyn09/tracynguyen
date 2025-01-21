e5b997208d49ade8a4528d6444b0353ad4864ddf
tracyn09 <tracyn0099@gmail.com>
2025-01-21 18:31:42 -0500

    Add files via upload
    Task 1– Employee Information

---
+++ cc_1.js
@@ -0,0 +1,10 @@
+//Task 1 Variables
+let employeeName = "Tracy Ngyuen"
+const employeeID = "U17147258"
+var  isActive = true
+console.log(typeof employeeName)
+console.log(typeof employeeID)
+console.log(typeof isActive)
+
+
+
