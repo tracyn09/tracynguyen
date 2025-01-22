ed0445684acd71ac99fb75d166a8a7f757a1d3b0
tracyn09 <tracyn0099@gmail.com>
2025-01-21 19:22:30 -0500

    Task 3– Number Data Type

--- cc_1.js
+++ cc_1.js
@@ -1,10 +1,20 @@
-//Task 2 Primitive Data Types
-let productName = "QuickPro"
-const productPrice = 34
-var  isAvailable = true
-console.log(typeof productName)
-console.log(typeof productPrice)
-console.log(typeof isAvailable)
+//Task 3 Number Data Type
+let accountBalance = 2457 
+console.log("Initial Balance: $" + accountBalance);
 
+//Cash Outflow 300
+accountBalance -= 300;
+console.log("After $300 Outflow: $" + accountBalance)
 
+//Cash Inflow 500
+accountBalance += 500;
+console.log("After $500 Inflow: $" + accountBalance)
+
+//3% Interest Applied
+accountBalance *= 1.03;
+console.log("After 3% Applied Interest: $" + accountBalance)
+
+//Splitting Balance // Dispersing Amongst Multiple Accounts
+accountBalance /= 2;
+console.log("After Splitting Balance: $" + accountBalance)
 
